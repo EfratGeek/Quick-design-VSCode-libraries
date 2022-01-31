42dde242bfbd34f0812e9d9461ee545db795eda6
EfratGeek <efrats.developer@gmail.com>
2022-01-31 12:20:32 +0200

    Update provider.ts

--- src/provider.ts
+++ src/provider.ts
@@ -1,8 +1,5 @@
 import * as vscode from 'vscode';
 
-
-
-
 export function provider() {
     const prov = vscode.languages.registerCompletionItemProvider('text', {
        provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
