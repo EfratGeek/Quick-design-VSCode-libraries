34ca3ce496c4a694c3a17626e2aee6963b18ac7c
EfratGeek <efrats.developer@gmail.com>
2022-05-01 14:50:12 +0300

    school

--- src/completion_class.ts
+++ src/completion_class.ts
@@ -2,37 +2,41 @@ import * as vscode from 'vscode';
 
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-export function class_c() {return vscode.languages.registerCompletionItemProvider(
-    'javascript',
-    {
-         provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+export function class_c() {
+    return vscode.languages.registerCompletionItemProvider(
+        'javascript',
+        {
+            provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
 
-            const commitCharacterCompletion = new vscode.CompletionItem('class=');
-			commitCharacterCompletion.commitCharacters = ['"'];
-			commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `=` to get `class`');
-            return[commitCharacterCompletion];
-          
-        }
-    },
- 
-);} 
+                const commitCharacterCompletion = new vscode.CompletionItem('class=');
+                commitCharacterCompletion.commitCharacters = ['"'];
+                commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `=` to get `class`');
+                return [commitCharacterCompletion];
+
+            }
+        },
+
+    );
+}
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-export function bootstrap_c(item:string) {return vscode.languages.registerCompletionItemProvider(
-    'html',
-    {
-         provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+export function bootstrap_c(item: string) {
+    return vscode.languages.registerCompletionItemProvider(
+        'html',
+        {
+            provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
 
-            const linePrefix = document.lineAt(position).text.substr(0, position.character);
-            if (!linePrefix.endsWith('class="')) {
-                return undefined;
+                const linePrefix = document.lineAt(position).text.substr(0, position.character);
+                if (!linePrefix.endsWith('class="')) {
+                    return undefined;
+                }
+                return [
+                    new vscode.CompletionItem(item, vscode.CompletionItemKind.Text),
+                    //new vscode.CompletionItem(item, vscode.CompletionItemKind.Text),
+
+                ];
             }
-            return [
-                new vscode.CompletionItem(item , vscode.CompletionItemKind.Text),
-                //new vscode.CompletionItem(item, vscode.CompletionItemKind.Text),
-
-            ];
-        }
-    },
-    '"'
-);} 
+        },
+        '"'
+    );
+} 
