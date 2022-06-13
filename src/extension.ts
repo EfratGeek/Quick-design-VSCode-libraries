6ba25dbc7bb05a91441e40ce67625237cd208cf1
EfratGeek <efrats.developer@gmail.com>
2022-06-13 12:56:59 +0300

    log

--- src/extension.ts
+++ src/extension.ts
@@ -12,15 +12,19 @@ export function activate() {
                 // const start: vscode.Position = new vscode.Position(position.line, 0);
                 // const range: vscode.Range = new vscode.Range(start, position);
                 // const text: string = document.getText(range);
+                // bring the line till the current Location
                 const linePrefix = document.lineAt(position).text.substring(0, position.character);
+                // check if match to class attributr.
                 const rawClasses: RegExpMatchArray | null = linePrefix.match(/class=["|']([\w- ]*$)/);
+                // if not return nothing.
                 if (!rawClasses) {
                     //|| rawClasses.length === 1
                     return [];
                 }
-
+                // if yes put in list the class if empty will not put anything.
                 let listClass = rawClasses[1].split(' ');
                 let bootListClass: string[] = [];
+                // put in new list jast classes existing in bootstrap.
                 listClass.forEach(_class =>{
                     if(CompletionClass.isInBootstrap(_class)){
                         bootListClass.push(_class);
@@ -28,18 +32,21 @@ export function activate() {
                 });
 
 				let _list: String[][]  = [];
-
+                // put in _list lists of completion.
 				for(let key in bootListClass){
 					_list.push(CompletionClass.getListByNumber(CompletionClass.getListOfBoot(bootListClass[key])));
 				}
 
+                // list of CompletionItem for CompletionList.
                 let compList:vscode.CompletionItem[] = [];
 
+                // chek on type tag thet user be.
                 CompletionClass.getListTags().forEach(tag =>{        
 
                     if(linePrefix.includes('<'+tag +' ')){       
                         let tagList = CompletionClass.getListByNumber(CompletionClass.getListOfTag(tag)); 
 						_list.push(tagList);
+                        // return the extend list of all lists.
 						let extendList = CompletionClass.getExtendList(_list);
 
                         for (let key in extendList){
@@ -50,11 +57,14 @@ export function activate() {
                     }
                 });  
 
+                // if do not know the tag.
                 if(compList.length === 0)
                 {
+                    // if the class is empty.
 					if(_list.length === 0){
 						_list.push(CompletionClass.getListByNumber(CompletionClass.getListOfTag('div')));
 					}
+                    // return the extend list of all lists.
 					let extendList = CompletionClass.getExtendList(_list);
                     for (let key in extendList){
                         let item = new vscode.CompletionItem(extendList[key],vscode.CompletionItemKind.Variable);
@@ -63,6 +73,7 @@ export function activate() {
                     }
                 }        
 
+                // send to user the class name for complate.
                 return new vscode.CompletionList(compList);
             }
         }
